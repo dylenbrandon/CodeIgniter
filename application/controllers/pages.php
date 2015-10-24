85e6193d4635fb5c27ee2b6d47091f48c00a6dea
Dylen Brandon Rivera <dylenbrivera@gmail.com>
2015-10-24 12:55:07 -0400

    PHP syntax errors

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,10 +1,20 @@
-<? php
+<?php
 
 class Pages extends CI_Controller {
 
 	public function view($page = 'home')
 	{
+	  if ( ! file_exists(APPPATH.'/views/pages/'.$page.'.php'))
+	  {
+	    //404 - Page not found
+	    show_404();
+	  }
 
+	$data['title'] = ucfirst($page); //Capitalize the first letter
+
+	$this->load->view('templates/header', $data);
+	$this->load->view('pages/'.$page, $data);
+	$this->load->view('templates/footer', $data);
+	
 	}
-
 }
